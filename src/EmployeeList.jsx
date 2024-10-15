80a1f1314b460d95838f4c5bd50303616739bcb2
Justin Zhu <justinzhu0913@gmail.com>
2024-10-15 12:00:43 -0400

    updated favicon

--- src/EmployeeList.jsx
+++ src/EmployeeList.jsx
@@ -55,12 +55,13 @@ const EmployeeList = () => {
     
     return (
         <div className="p-6">
-          <h1 className="text-3xl font-bold mb-4">Employee List</h1>
+          <h1 class="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-teal-500 to-green-500
+       uppercase tracking-wider my-6 mb-12 text-center">{'{Employee List}'}</h1>
           <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
             {displayedEmployees.map(employee => (
               <div key={employee.id} 
               style={{ backgroundColor: employeeColors[employee.id] }}
-                className="p-4 rounded-lg shadow-md hover:shadow-xl scale-[1] hover:scale-[1.03] transition duration-300 ease-in-out">
+                className="p-4 rounded-xl shadow-lg hover:shadow-xl scale-[1] hover:scale-[1.03] transition duration-300 ease-in-out">
                 <p className="text-lg font-bold">{employee.name}</p>
                 <p className="text-sm">{employee.position}</p>
               </div>
